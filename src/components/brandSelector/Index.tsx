3a653cd19d7aaea04d11464e4256055b243f80ff
Chouhangarv <chouhangoutam147@gmail.com>
2023-08-18 17:14:54 +0530

    font family

--- src/components/brandSelector/Index.tsx
+++ src/components/brandSelector/Index.tsx
@@ -7,10 +7,13 @@ import { constantImages } from '../../assets/images/Images'
 export default function BrandSelect() {
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Image source={constantImages.landingBackground} style={styles.bg}></Image>
-
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Image resizeMode='stretch' source={constantImages.landingBackground} style={styles.bg}></Image>
+     <Image source={constantImages.dinner} style={styles.dinner}></Image>
+      <View style={styles.BrandSelect}>
+      <Text style={{fontFamily:"Poppins-SemiBold"}}>Hello</Text>
+      </View>
+    </View>
   )
 }
 
